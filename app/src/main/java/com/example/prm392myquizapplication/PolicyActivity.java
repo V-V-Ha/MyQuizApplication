928d73ad5c64a9bbb445e37f2711c7da147f9fbf
trghoangming <trghoangming@gmail.com>
2024-03-20 01:11:44 +0700

    add policy

---
+++ app/src/main/java/com/example/prm392myquizapplication/PolicyActivity.java
@@ -0,0 +1,19 @@
+package com.example.prm392myquizapplication;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+
+
+public class PolicyActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.policy);
+        }
+    }
+
